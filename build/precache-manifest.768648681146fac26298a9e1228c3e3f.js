4d0cff4b3eebf572c18e9d6c43085b5e173fd4d0
Danilo Mezgec <mezgec.danilo@gmail.com>
2019-12-14 19:50:19 +0100

    chore: build added to repo

---
+++ build/precache-manifest.768648681146fac26298a9e1228c3e3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "579f778037da0de750e3",
+    "url": "/static/js/main.579f7780.chunk.js"
+  },
+  {
+    "revision": "67749fc6e4d747995420",
+    "url": "/static/js/1.67749fc6.chunk.js"
+  },
+  {
+    "revision": "579f778037da0de750e3",
+    "url": "/static/css/main.26b28477.chunk.css"
+  },
+  {
+    "revision": "0a875ec625031a0222a564bba4ca450f",
+    "url": "/index.html"
+  }
+];
